b69a420c3bbc8eb5ff59d48bdbaef8bcf27448d9
Mateusz Podeszwa <mateuszpodeszwa@icloud.com>
2023-11-20 18:54:19 +0000

    The Variable Shop commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Build a program with a variable of all 14 types
+ * Assign each of them a value
+ * Use Console.WriteLine to display them
+ * 
+ */
 
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace The_Variable_Shop
+{
+    class PartOneTypes
+    {
+        static void Main()
+        {
+            byte @byte = 12;
+            short @short = 31;
+            int @int = 32;
+            long @long = 5645;
+            sbyte @sbyte = 64;
+            ushort @ushort = 45;
+            uint @uint = 542;
+            ulong @ulong = 23;
+            string @string = "Hello";
+            char @char = '^';
+            float @float = 345.43f;
+            double @double = 876.865345;
+            decimal @decimal = 534255.345667456m;
+            
+            Console.WriteLine($"{@byte}, {@decimal}, {@short}, {@int}, {@long}, {@sbyte}, {@ushort}, {@uint}" +
+                              $"{@ulong}, {@string}, {@char}, {@float}, {@double}");
+        }
+    }
+}
+
+
