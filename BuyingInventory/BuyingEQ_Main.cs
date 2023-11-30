63e332ca7987309603a946b246605c0b1272ca72
Mateusz Podeszwa <mateuszpodeszwa@icloud.com>
2023-11-30 17:40:48 +0000

    Changed file name

---
+++ BuyingInventory/BuyingEQ_Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyCSharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            //This is part 1 of the task "Buying Inventory"
+
+            Console.WriteLine(@"The following items are available:
+                  1  -  Rope
+                  2  -  Torches
+                  3  -  Climbing Equipment
+                  4  -  Clean Water
+                  5  -  Machete
+                  6  -  Canoe
+                  7  -  Food Supplies
+                    
+                  What number do you want to see the price of?
+                  ");
+            Console.Write("The number: ");
+            sbyte theItem = sbyte.Parse(Console.ReadLine());
+
+            string response = theItem switch
+            {
+                1 => "Rope: 10$",
+                2 => "Torches: 15$",
+                3 => "Climbing Equipment: 25$",
+                4 => "Clean Water: 1$",
+                5 => "Machete: 20$",
+                6 => "Canoe: 200$",
+                7 => "Food Supplies: 1$",
+                _ => "We couldn't find the product.."
+            };
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(response);
+        }
+    }
+}
