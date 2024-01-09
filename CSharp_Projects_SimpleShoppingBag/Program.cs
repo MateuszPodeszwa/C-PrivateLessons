7480029768fd1121054fa7a2d3c8ecbbb73fad9a
Mateusz Podeszwa <podinatubie@gmail.com>
2024-01-09 15:38:29 +0000

    Clear start of the project

--- CSharp_Projects_SimpleShoppingBag/Program.cs
+++ CSharp_Projects_SimpleShoppingBag/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace CSharp_Projects_SimpleShoppingBag;
+﻿using Figgle;
+
+namespace CSharp_Projects_SimpleShoppingBag;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
     }
 }
